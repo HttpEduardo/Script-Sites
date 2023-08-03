265228980cb70f5dd141a51ef1da2e2aedf30c45
Eduardo <101486227+HttpEduardo@users.noreply.github.com>
2023-08-02 23:08:30 -0300

    Create site.py

---
+++ site.py
@@ -0,0 +1,23 @@
+import os
+
+# Crie uma pasta para o site
+os.mkdir("website")
+
+# Crie um arquivo index.html na pasta
+with open("website/index.html", "w") as f:
+    f.write("<html>\n<head>\n<title>Meu site</title>\n</head>\n<body>\n<h1>Olá, mundo!</h1>\n</body>\n</html>")
+
+# Crie um arquivo css para o site
+with open("website/style.css", "w") as f:
+    f.write("body { font-family: sans-serif; }\n")
+
+# Crie um arquivo 404.html para o site
+with open("website/404.html", "w") as f:
+    f.write("<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\n<h1>Página não encontrada</h1>\n</body>\n</html>")
+
+# Crie um arquivo robots.txt para o site
+with open("website/robots.txt", "w") as f:
+    f.write("User-agent: *\nDisallow: /")
+
+# Abra o site no navegador
+os.system("open website/index.html")
